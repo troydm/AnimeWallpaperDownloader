9fb439618d17db4a095384fcf92963e7cb7a4fc7
Dmitry Geurkov <dmitry_627@mail.ru>
2011-11-27 21:23:44 +0400

    Initial commit

--- AnimeWallpaperDownloader/AppDelegate.rb
+++ AnimeWallpaperDownloader/AppDelegate.rb
@@ -8,8 +8,45 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :tags
+    attr_accessor :size
+    attr_accessor :number
+    attr_accessor :saveInto
+    attr_accessor :startButton
+    attr_accessor :output
+    
     def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+        @startButton.setEnabled(false)
+        @saveInto.stringValue = NSHomeDirectory()+"/Pictures"
+    end
+    
+    def controlTextDidChange(notification)
+        sender = notification.object
+        if sender == tags
+            @startButton.setEnabled(@tags.stringValue.size > 0)
+        elsif sender == number
+            begin
+                @number.setIntValue(@number.intValue)
+                if @number.intValue < 0
+                    @number.setIntValue(-@number.intValue)
+                elsif @number.intValue == 0
+                    @number.setIntValue(20)
+                end
+            rescue
+                @number.setIntValue(20)
+            end            
+        end
+    end
+    
+    def browse(sender)
+        dialog = NSOpenPanel.openPanel
+        dialog.canChooseFiles = false
+        dialog.canChooseDirectories = true
+        dialog.allowsMultipleSelection = false
+        
+        if dialog.runModalForDirectory(nil, file:nil) == NSOKButton
+            @saveInto.stringValue = dialog.filenames.first
+        end
     end
 end
 
